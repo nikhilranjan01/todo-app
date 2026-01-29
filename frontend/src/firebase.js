a4ee1cd27b35c55ef16d46ba5e15e83c5623e86b
nikhilranjan01 <nikhilranjan2004@gmail.com>
2026-01-29 16:08:19 +0530

    Configure Firebase authentication

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTx6ANTO8qHJEkm-VvqXQQ46JalYJ1U8U",
+  authDomain: "todo-fullstack-app-b93b5.firebaseapp.com",
+  projectId: "todo-fullstack-app-b93b5",
+  storageBucket: "todo-fullstack-app-b93b5.firebasestorage.app",
+  messagingSenderId: "174904227829",
+  appId: "1:174904227829:web:9eec86cfbb034ce58192bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
